89b4f83711489dff9380d2a78f4b4d2fd440f535
Louis <maliyp@cs.washington.edu>
2021-08-29 01:14:36 -0700

    Updates

---
+++ precache-manifest.aa50b751f06274e256ff58e4f8763bb4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82bd59442f891b1d6cba24eeab4de16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da20a7747890675652db",
+    "url": "/static/css/2.af22ade3.chunk.css"
+  },
+  {
+    "revision": "1202cb901e9f6411e0f5",
+    "url": "/static/css/main.e99179f6.chunk.css"
+  },
+  {
+    "revision": "da20a7747890675652db",
+    "url": "/static/js/2.1d8b33b5.chunk.js"
+  },
+  {
+    "revision": "08ec23b80584629a64e9f1ba34d73357",
+    "url": "/static/js/2.1d8b33b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1202cb901e9f6411e0f5",
+    "url": "/static/js/main.d9881ed7.chunk.js"
+  },
+  {
+    "revision": "c26461e9135c89036065",
+    "url": "/static/js/runtime-main.059fb47f.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
